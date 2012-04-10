4f3707149bb044dc463f80fc4d4f7e8811a84a1e
Yungchih Chen <yungchih_chen@yahoo.com>
2012-04-10 11:27:07 -0400

    add validation on product model and add redirect_to to product list page after added comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
 	has_many :categorizations
 	has_many :categories, through: :categorizations
 	has_many :comments
+
+	validates :name, :presence => true
+	validates :price, :presence => true, :numericality => true
 end
